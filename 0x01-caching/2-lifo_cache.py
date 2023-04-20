dd2733f85a9c0fd4ac0911a327eb7d49197a4138
Adams Adjartey <kaadjartey@gmail.com>
2023-04-20 00:45:08 +0000

    2. LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -29,16 +29,16 @@ class LIFOCache(BaseCaching):
                self.evict()
            self.queue.append(key)
            self.cache_data[key] = item
-                                                                                          def get(self, key):
+   def get(self, key):
        """ Must return the value in self.cache_data linked to key.
        """
        return self.cache_data.get(key, None)
-                                                                                          def is_full(self):
+   def is_full(self):
        """ If the number of items in self.cache_data is higher that
        BaseCaching.MAX_ITEMS
        """
        return len(self.cache_data) >= self.MAX_ITEMS
-                                                                                          def evict(self):
+   def evict(self):
        """ you must print DISCARD: with the key discarded and following by a
        new line -pop-
        """
