077df906179732d6963e58d38701d44365aacfdf
Adams Adjartey <kaadjartey@gmail.com>
2023-04-20 03:10:15 +0000

    2. LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
-""" LIFO Cache """
+""" 2. LIFO Caching
+"""
+from datetime import datetime
+BaseCaching = __import__('base_caching').BaseCaching
 
-from base_caching import BaseCaching
-BaseCaching = __import__("base_caching").BaseCaching
 
 class LIFOCache(BaseCaching):
-    """ This class represent a LIFO caching system """
-
+    """ BaseCache defines:
+    - overwrite functions 'put' and 'get' for implement
+    LIFO caching system
+    """
     def __init__(self):
-        """ Initialize class instance """
+        """ Initiliaze
+        """
         super().__init__()
-        self.key_order = []
+        self.cache_by_time = {}
 
-   def remove_last_item(self):
-       """ Remove the last item from cache """
-       lifo_key = self.key_order.pop()
-       del self.cache_data[lifo_key]
-       print("DISCARD: {}".format(lifo_key))
-
-   def put(self, key, item):
-       """ Add an item in the cache """
-       if key is None or item is None:
-           return
-       if len(self.cache_data) >= self.MAX_ITEMS:
-           self.remove_last_item()
-       self.cache_data[key] = item
-       self.key_order.append(key)
-                                                                                                                                                   def get(self, key):
-       """ Get an item from the cache """
-       if key is None or key not in self.cache_data:
-           return None
-       self.key_order.remove(key)
-       self.key_order.append(key)
-       return self.cache_data[key]
+    def put(self, key, item):
+        """
+        Assign to the dictionary self.cache_data the item
+        value for the key key
+        If the number of items in self.cache_data is higher
+        that BaseCaching.MAX_ITEMS:
+        - must discard the last item put in cache (LIFO algorithm)
+        - must print DISCARD: with the key discarded and following by
+        a new line
+        """
+                                                                                                                                                        if key and item:
+            self.cache_by_time[key] = datetime.now()
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                sorted_dict_keys = sorted(
+                                          self.cache_by_time,
+                                          key=self.cache_by_time.get)
+                penultimate_in__key_element = sorted_dict_keys[-2]
+                del self.cache_by_time[penultimate_in__key_element]
+                del self.cache_data[penultimate_in__key_element]
+                print('DISCARD: {}'.format(penultimate_in__key_element))
+                                                                                                                                                     def get(self, key):
+         """
+         Return the value in self.cache_data linked to key
+         """
+         return self.cache_data.get(key)
