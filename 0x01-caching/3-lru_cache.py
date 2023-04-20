79af457aeef3e14f99feb3565035e97f5138c04f
Adams Adjartey <kaadjartey@gmail.com>
2023-04-20 02:17:45 +0000

    3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
-""" LRU Caching """
+"""
+LRU caching system
+"""
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    """
+    LRU cache implementation
+    """
+
     def __init__(self):
+        """
+        Initialize
+        """
         super().__init__()
-        self.head, self.tail = '-', '='
-        self.next, self.prev = {}, {}
-        self.handle(self.head, self.tail)
+        self.lru_keys = []
 
-    def handle(self, head, tail):
-        """ LRU algorithm, handle elements """
-        self.next[head], self.prev[tail] = tail, head
+    def put(self, key, item):
+        """
+        Add an item to the cache
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS and key not in self.cache_data:
+            lru_key = self.lru_keys.pop(0)
+            del self.cache_data[lru_key]
+            print("DISCARD:", lru_key)
 
-  def _remove(self, key):
-      """ LRU algorithm, remove element """
-      self.handle(self.prev[key], self.next[key])
-      del self.prev[key], self.next[key], self.cache_data[key]
+        self.cache_data[key] = item
+        if key in self.lru_keys:
+            self.lru_keys.remove(key)
+        self.lru_keys.append(key)
+                                                                                                                            def get(self, key):
+        """
+        Get an item from the cache
+        """
+        if key is None or key not in self.cache_data:
+            return None
 
-  def _add(self, key, item):
-      """ LRU algorithm, add element """
-      self.cache_data[key] = item
-      self.handle(self.prev[self.tail], key)
-      self.handle(key, self.tail)
-      if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-          print("DISCARD: {}".format(self.next[self.head]))
-          self._remove(self.next[self.head])
-
-  def put(self, key, item):
-      """ Assign to the dictionary """
-      if key and item:
-          if key in self.cache_data:
-              self._remove(key)
-          self._add(key, item)
-                                                                                                                          def get(self, key):
-      """ Return the value linked """
-      if key is None or self.cache_data.get(key) is None:
-          return None
-      if key in self.cache_data:
-          value = self.cache_data[key]
-          self._remove(key)
-          self._add(key, value)
-          return value
+        self.lru_keys.remove(key)
+        self.lru_keys.append(key)
+        return self.cache_data[key]
